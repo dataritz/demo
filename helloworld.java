3d1bb8c931ee1af3712847604a31f9f9c628938b
dataritz <gopalgupta@abes.ac.in>
2020-04-09 18:30:03 +0530

    changes

--- helloworld.java
+++ helloworld.java
@@ -5,7 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
-		System.out.println("Updated");
+		System.out.println("Upd");
 	}
 
 }
